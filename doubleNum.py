d756765c2fc5c18acf1a4c3c6a794737ac7a5d2a
Hannah <148793520+Developingcat@users.noreply.github.com>
2025-01-06 14:46:23 +0000

    doubleNumbers done

---
+++ doubleNum.py
@@ -0,0 +1,20 @@
+"""
+Input a series of numbers and see what their power of two results within. 
+"""
+
+def pow2(y):
+    powOf2 = y ** 2
+    # return powOf2
+    print(y, "to the 2nd power is: ", powOf2)
+
+
+myNumbers = [2, 5, 12, 17, 25, 32]
+
+
+for i in myNumbers: 
+    pow2(i)
+    
+
+# pow2(2)
+
+print("Our function is complete.")
